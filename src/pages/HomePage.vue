4f7dee94e815786c358df9c0fe1f289e36a59fdb
Fatih <fatih.unlu0341@gmail.com>
2018-07-06 18:28:39 +0300

    BlogPage links added

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -9,7 +9,7 @@
                 <div class="card-body">
                 <h2 class="card-title">Post Title</h2>
                 <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
-                <a href="#" class="btn btn-primary">Read More →</a>
+                <router-link class="btn btn-primary" :to="{ name: 'BlogPage' }">Read More</router-link>
                 </div>
                 <div class="card-footer text-muted">
                 Posted on January 1, 2017 by
@@ -21,7 +21,7 @@
                 <div class="card-body">
                 <h2 class="card-title">Post Title</h2>
                 <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
-                <a href="#" class="btn btn-primary">Read More →</a>
+                <router-link class="btn btn-primary" :to="{ name: 'BlogPage' }">Read More</router-link>
                 </div>
                 <div class="card-footer text-muted">
                 Posted on January 1, 2017 by
@@ -33,7 +33,7 @@
                 <div class="card-body">
                 <h2 class="card-title">Post Title</h2>
                 <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
-                <a href="#" class="btn btn-primary">Read More →</a>
+                <router-link class="btn btn-primary" :to="{ name: 'BlogPage' }">Read More</router-link>
                 </div>
                 <div class="card-footer text-muted">
                 Posted on January 1, 2017 by
